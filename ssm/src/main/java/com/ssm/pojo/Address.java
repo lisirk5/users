f2956557e2cb8b76cde519841b545975a6bbed3f
user <user@qq.com>
2020-07-02 14:17:50 +0800

    今天代码

---
+++ ssm/src/main/java/com/ssm/pojo/Address.java
@@ -0,0 +1,18 @@
+package com.ssm.pojo;
+
+
+import java.util.Date;
+
+public class Address {
+
+    private    Integer id;//'主键ID',
+    private  String contact;//'联系人姓名',
+    private   String addressDesc;// '收货地址明细',
+    private  String postCode;// '邮编',
+    private  String tel;//'联系人电话',
+    private String createdBy;// '创建者',
+    private  Date creationDate;//创建时间',
+    private  String modifyBy;//'修改者',
+    private  Date modifyDate;// '修改时间',
+    private   String userId;//用户ID',
+}
